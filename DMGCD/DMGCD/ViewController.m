4242afca0a6c9c79558aff61e2ed153718d35d63
leoliuyt <leoliuyt@sina.cn>
2017-09-12 17:41:46 +0800

    add

--- DMGCD/DMGCD/ViewController.m
+++ DMGCD/DMGCD/ViewController.m
@@ -26,9 +26,11 @@ static NSInteger kPerValue = 1;
 }
 
 - (IBAction)startAction:(id)sender {
+    ////https://developer.apple.com/library/content/documentation/General/Conceptual/ConcurrencyProgrammingGuide/GCDWorkQueues/GCDWorkQueues.html#//apple_ref/doc/uid/TP40008091-CH103-SW22
 //    [self demo1];
 //    [self demo2];
-    [self demo3];
+//    [self demo4];
+    [self demo5];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -141,5 +143,88 @@ static NSInteger kPerValue = 1;
     
     //开始执行
     dispatch_resume(timerSource);
+}
+
+- (void)demo4
+{
+    NSString *path = [[NSBundle mainBundle] pathForResource:@"aa" ofType:@"json"];
+    NSData *data = [path dataUsingEncoding:NSUTF8StringEncoding];
+    char *buf = (char *)[data bytes];
+    ProcessContentsOfFile(buf);
+    
+}
+
+
+dispatch_source_t ProcessContentsOfFile(const char* filename)
+{
+    // Prepare the file for reading.
+    int fd = open(filename, O_RDONLY);
+    if (fd == -1){return NULL;}
+    fcntl(fd, F_SETFL, O_NONBLOCK); // Avoid blocking the read operation
+    dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
+    dispatch_source_t readSource = dispatch_source_create(DISPATCH_SOURCE_TYPE_READ, fd, 0, queue);
+    if (!readSource)
+    {
+        close(fd);
+        return NULL;
+    }
+    // Install the event handler
+    dispatch_source_set_event_handler(readSource, ^{
+        size_t estimated = dispatch_source_get_data(readSource) + 1;
+        char* buffer = (char*)malloc(estimated);
+        if (buffer)
+        {
+            ssize_t actual = read(fd, buffer, (estimated));
+            if(actual > 0){
+                NSLog(@"Got data from stdin: %.*s", (int)actual, buffer);
+            }
+        }
+    });
+    dispatch_source_set_cancel_handler(readSource, ^{close(fd);}); // Install the cancellation handler
+    dispatch_resume(readSource); // Start reading the file.
+    
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        
+    });
+    return readSource;
+}
+
+- (void)demo5
+{
+    NSString *path = [[NSBundle mainBundle] pathForResource:@"aa" ofType:@"json"];
+    NSData *data = [path dataUsingEncoding:NSUTF8StringEncoding];
+    char *buf = (char *)[data bytes];
+    WriteDataToFile(buf);
+}
+
+dispatch_source_t WriteDataToFile(const char* filename)
+{
+    int fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, (S_IRUSR | S_IWUSR | S_ISUID | S_ISGID));
+    if (fd == -1)
+        return NULL;
+    fcntl(fd, F_SETFL); // Block during the write.
+    dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
+    dispatch_source_t writeSource = dispatch_source_create(DISPATCH_SOURCE_TYPE_WRITE, fd, 0, queue);
+    if (!writeSource)
+    {
+        close(fd);
+        return NULL;
+    }
+    dispatch_source_set_event_handler(writeSource, ^{
+        NSString *str = @"hello world";
+        NSData *data = [str dataUsingEncoding:NSUTF8StringEncoding];
+        char *buf = (char *)[data bytes];
+        size_t bufferSize = sizeof(buf);
+        void* buffer = malloc(bufferSize);
+        ssize_t a = write(fd, buffer, bufferSize);
+        if (a == -1) {
+             NSLog(@"error: %s(errno: %d)",strerror(errno),errno);
+        }
+        free(buffer);
+        dispatch_source_cancel(writeSource); // Cancel and release the dispatch source when done.
+    });
+    dispatch_source_set_cancel_handler(writeSource, ^{close(fd);});
+    dispatch_resume(writeSource);
+    return (writeSource);
 }
 @end
